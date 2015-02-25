c9f25cd252bcd7b45e48ed90241b7720f6b542f0
smfung <ming321fung@gmail.com>
2015-02-25 13:02:11 +0800

    Update FriendsPost.java

--- LAB/src/base/FriendsPost.java
+++ LAB/src/base/FriendsPost.java
@@ -1,6 +1,16 @@
 package base;
+import java.util.Date;
+public class FriendsPost extends post{
 
-public class FriendsPost extends post {
 	private User friend;
+	public FriendsPost (Date date,String content){
+		super(date,content);
+		
+	}
+	public FriendsPost (Date date,String content,User user){
+		super(date,content);
+		this.user = user;
+		
+	}
 
 }
