c77195e032cd4b79233d6761a3b286e11c18c587
smfung <ming321fung@gmail.com>
2015-02-25 13:02:54 +0800

    Update User.java

--- LAB/src/base/User.java
+++ LAB/src/base/User.java
@@ -1,5 +1,16 @@
 package base;
-
+import java.util.Date;
 public class User {
+	private int id;
+	private String name;
+	private String address;
+	
+	public User (int id, String name, String address){
+		this.id=id;
+		this.address=address;
+		this.name=name;
+		System.out.println("User [userID="+id+", userName="+name+", userEmail="+address+"]");
+	}
+	
 
 }
