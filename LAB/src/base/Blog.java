07b50ebd27d9a4129f020165005d3579baf0dd8c
smfung <ming321fung@gmail.com>
2015-03-04 12:48:49 +0800

    Update Blog.java

--- LAB/src/base/Blog.java
+++ LAB/src/base/Blog.java
@@ -1,6 +1,7 @@
 package base;
 
 import java.util.ArrayList;
+
 import base.*;
 public class Blog {
 	private User user;
@@ -8,7 +9,7 @@ public class Blog {
 	
 	public Blog(User user){
 		this.user=user;
-		this.allPost=new ArrayList<Post>();
+		this.allPost=new ArrayList<post>();
 	}
 	
 	public void setUser(User user){
@@ -19,14 +20,17 @@ public class Blog {
 		return this.user;
 	}
 	
-	public void post(Post p){
-		this.allPost.add(p);
-		System.out.println("add post succeed");
+	public void post(post p){
+		allPost.add(p);
+		System.out.println("A new Post:\n"+p);
 	}
 	
 	public void list(){
-		for(int a:this.allPost){
-			allPost.get(a).toSting();
+		System.out.println("Current posts");
+		for(int a=0 ; a<allPost.size();a++){
+			int i=a+1;
+			System.out.println("Post["+ i +"]:"+allPost.get(a));
+			
 		}
 	}
 	
@@ -37,6 +41,43 @@ public class Blog {
 			allPost.remove(index);
 			
 	}
+
+	@Override
+	public String toString() {
+		return "Blog [user=" + user + ", allPost=" + allPost + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((allPost == null) ? 0 : allPost.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Blog other = (Blog) obj;
+		if (allPost == null) {
+			if (other.allPost != null)
+				return false;
+		} else if (!allPost.equals(other.allPost))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
 	
 
 }
+
